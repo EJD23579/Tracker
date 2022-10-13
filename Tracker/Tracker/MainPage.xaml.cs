7c8df7a4e2ef5bcbce48b2999bb3ddb91ddcd6ed
Elliot-Dorey <dorey.elliot@gmail.com>
2022-10-13 15:32:25 +0100

    First Commit to Development branch, maps implemented

--- Tracker/Tracker/MainPage.xaml.cs
+++ Tracker/Tracker/MainPage.xaml.cs
@@ -1,20 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Xml.Serialization;
+using TcxTools;
+using Tracker;
+using Xamarin.Essentials;
 using Xamarin.Forms;
+using Xamarin.Forms.Maps;
 
 namespace Tracker
 {
     public partial class MainPage : ContentPage
     {
+        
+
         public MainPage()
         {
             InitializeComponent();
+
+            
+           
+            
+
+           
         }
 
-        
+       
     }
 }
